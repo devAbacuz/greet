1cbdb4c089f55d8e19024dcb9af3facb6192a709
devAbacuz <cto.abacuz@gmail.com>
2024-06-26 19:39:52 +0530

    This is the message sub
    This is the message body

--- src/main/java/com/abacuz/greet/model/GreetingService.java
+++ src/main/java/com/abacuz/greet/model/GreetingService.java
@@ -1,16 +1,21 @@
 package com.abacuz.greet.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.abacuz.greet.service.NameServiceClient;
 import com.abacuz.greet.view.GreetingView;
 
 @Service
 public class GreetingService {
+	
+	@Autowired
+	//private NameServiceClient nameService;
 
 	public GreetingView greet() {
 		GreetingView view = new GreetingView();
-		view.setGreet("Hiii");
-		view.setName("Jomy");
+		view.setGreet("Hiii my");
+		view.setName("Gulmi Baby!!!!!");
 		return view;
 	}
 
